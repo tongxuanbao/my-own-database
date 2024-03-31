ff93b9a04c789e8dab9402c64a038bc256923188
Ryan Tong <baotongxuan@gmail.com>
2024-03-31 23:59:22 +1100

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tongxuanbao/my-own-database
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+type BNode struct {
+	data []byte
+}
+
+const (
+	BNODE_NODE = 1 // internal nodes without values
+	BNODE_LEAF = 1 // leaf nodes with values
+)
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+	// callbacks for managing on-disk pages
+	get func(uint64) BNode // dereference a pointer
+	new func(BNode) uint64 // allocate a new page
+	del func(uint64)       // deallocate a page
+}
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE)
+}
+
+func main() {
+
+}
