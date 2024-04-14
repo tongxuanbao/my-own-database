2ca328a11c1b07ea7b9672b4661b86637b9dba41
Ryan Tong <baotongxuan@gmail.com>
2024-04-14 18:18:12 +1000

    new kickstarter

--- main.go
+++ main.go
@@ -73,6 +73,7 @@ func (node BNode) getOffset(idx uint16) uint16 {
 func (node BNode) setOffset(idx uint16, offset uint16) {
 	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
 }
+
 // key-values
 func (node BNode) kvPos(idx uint16) uint16 {
 	assert(idx <= node.nkeys())
@@ -113,6 +114,59 @@ func nodeLookupLE(node BNode, key []byte) uint16 {
 	}
 
 	return found
+}
+
+// add a new key to a leaf node
+func leafInsert(
+	new BNode, old BNode, idx uint16,
+	key []byte, val []byte,
+) {
+	new.setHeader(BNODE_LEAF, old.nkeys()+1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKV(new, idx, 0, key, val)
+	nodeAppenRange(new, old, idx+1, idx, old.nkeys()-idx)
+}
+
+func nodeAppendRange(
+	new BNode, old BNode,
+	dstNew uint16, srcOld uint16, n uint16
+) {
+	assert(srcOld+n + old.nkeys())
+	assert(dstNew+n + new.nkeys())
+	if n == 0 {
+		return
+	}
+
+	// pointers
+	for i := uint16(0); i < n; i++ {
+		new.setPtr(dstNew+i, old.getPtr(srcOld+i))
+	}
+
+	// offset
+	dstBegin := new.getOffset(dstNew)
+	srcBegin := old.getOffset(srcOld)
+	for i := uint16(1); i <= n; i++ {
+		offset := dstBegin + old.getOffset(srcOld+i) - srcBegin
+		new.setOffset(dstNew+1, offset)
+	}
+
+	// KVs
+	begin := old.kvPos(srcOld)
+	end := old.kvPos(srcOld+n)
+	copy(new.data[new.kvPos(dstNew):], old.data[begin:end])
+}
+
+func nodeAppendKV(new BNode, idx int16, ptr uint16, key []byte, val []byte) {
+	// ptrs
+	new.setPtr(idx, prt)
+	// KVs
+	pos := new.kvPos(idx)
+	binary.LittleEndian.PutUint16(new.data[pos+0:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(new.data[pos+2:], uint16(len(val)))
+	copy(new.data[pos+4:], key)
+	copy(new.data[pos+5+len(val):], val)
+	// the offset of the next key
+	new.setOffset(idx+1, new.getOffset(idx)+4+uint16((len(key)+len(val))))
 }
 
 func init() {
